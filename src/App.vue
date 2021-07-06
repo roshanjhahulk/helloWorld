28a554a70e0a1af0d7891b3b3d8b8923c9a3cc20
roshan <home@roshans-MacBook-Air.local>
2021-07-06 18:56:56 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <nav id="navbar-top" class="navbar navbar-light ">
+    <div class="container-fluid">
+      <a class="navbar-brand wh-color f-20">Countries</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search countries" aria-label="Search">
+        <button id="submit-bttn" class="btn btn-outline-success wh-color mx-2" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+  
+  <div class="row align-items-center d-flex m-5">
+   <Card ></Card>
+  </div>
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Card from './components/Card.vue'
 export default {
+  components: { Card },
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  component : {
+    Card,
+  },
+  
 }
 </script>
 
@@ -21,6 +36,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+#navbar-top{
+  background: rgb(48, 119, 48);
+}
+
+.wh-color{
+  color: white !important;
+}
+
+.f-20{
+  font-size: 25px !important;
+}
+
+#submit-bttn{
+  background:rgb(0, 51, 0);
+  
+}
+
 </style>
